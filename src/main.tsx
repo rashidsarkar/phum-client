d0f4d3d795c23121f78109facdb01df3223d26f5
rashidsarker <rashidrock558@gmail.com>
2025-01-10 00:42:20 +0600

    26.5 start

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./app.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes.tsx";
 
-render(<App />, document.getElementById('app')!)
+const root = ReactDOM.createRoot(document.getElementById("app")!);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
