639ad7597873f404fa8f5760f992d4800b03458c
rashidsarker <rashidrock558@gmail.com>
2025-01-23 15:13:10 +0600

    31.1 end

--- src/pages/admin/CourseManagement/SemesterRegistration.tsx
+++ src/pages/admin/CourseManagement/SemesterRegistration.tsx
@@ -1,14 +1,14 @@
 import PHFroom from "../../../components/form/PHFroom";
 import { FieldValues, SubmitErrorHandler } from "react-hook-form";
-import { Button, Col, Flex } from "antd";
+import { Button, Col, Flex, Row } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
-import { monthOptions } from "../../../constants/global";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
 import { toast } from "sonner";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 import { semesterStatusOptions } from "../../../constants/semester";
 import PHDatePicker from "../../../components/form/PHDatePicker";
+import PHInput from "../../../components/form/PHInput";
+import { useAddRegisteredSemesterMutation } from "../../../redux/features/admin/courseManagement";
+import { TResponse } from "../../../types";
 
 const nameOptions = [
   {
@@ -26,6 +26,7 @@ const nameOptions = [
 ];
 
 export default function SemesterRegistration() {
+  const [addRegisteredSemester] = useAddRegisteredSemesterMutation();
   const { data: academicSemester } = useGetAllSemestersQuery([
     { name: "sort", value: "year" },
   ]);
@@ -35,45 +36,64 @@ export default function SemesterRegistration() {
       label: `${item.name} ${item.year}`,
     };
   });
-  console.log(academicSemester);
+  // console.log(academicSemester);
   const onSubmit: SubmitErrorHandler<FieldValues> = async (data) => {
-    // let toastID = toast.loading("Creating Academic Semester");
-
+    let toastID = toast.loading("Creating Academic Semester");
+    // console.log(semesterData);
     const semesterData = {
       ...data,
+      minCredit: Number(data.minCredit),
+      maxCredit: Number(data.maxCredit),
     };
+
     console.log(semesterData);
-    // try {
-    //   const res = (await addAcademicSemester(semesterData)) as TResponse;
-    //   if (res.error) {
-    //     toast.error(res.error.data.message, { id: toastID });
-    //   } else {
-    //     toast.success("Academic Semester Create Successfully", { id: toastID });
-    //   }
-    //   console.log(res);
-    // } catch (error) {
-    //   console.log(error);
-    //   toast.error("Error Creating Academic Semester", { id: toastID });
-    // }
+    try {
+      const res = (await addRegisteredSemester(semesterData)) as TResponse<any>;
+
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastID });
+      } else {
+        toast.success("Academic Semester Create Successfully", { id: toastID });
+      }
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+      toast.error("Error Creating Academic Semester", { id: toastID });
+    }
   };
 
   return (
     <Flex justify="center" align={"center"}>
-      <Col span={6}>
+      <Col span={24}>
         <PHFroom onSubmit={onSubmit}>
-          <PHSelect
-            label={"Academic Semester"}
-            name={"academicSemester"}
-            options={academicSemesterOptions}
-          />
-          <PHSelect
-            label={"Status"}
-            name={"status"}
-            options={semesterStatusOptions}
-          />
-
-          <PHDatePicker name="startDate" label="Start Date" />
-          <PHDatePicker name="endDate" label="End Date" />
+          <Row gutter={6}>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+              <PHSelect
+                label={"Academic Semester"}
+                name={"academicSemester"}
+                options={academicSemesterOptions}
+              />
+            </Col>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+              <PHSelect
+                label={"Status"}
+                name={"status"}
+                options={semesterStatusOptions}
+              />
+            </Col>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+              <PHDatePicker name="startDate" label="Start Date" />
+            </Col>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+              <PHDatePicker name="endDate" label="End Date" />
+            </Col>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+              <PHInput type="text" name="minCredit" label="Min Credit" />
+            </Col>
+            <Col span={24} lg={{ span: 12 }} md={{ span: 12 }}>
+              <PHInput type="text" name="maxCredit" label="Max Credit" />
+            </Col>
+          </Row>
           <Button htmlType="submit">Submit</Button>
         </PHFroom>
       </Col>
---
+++ src/redux/features/admin/courseManagement.ts
@@ -0,0 +1,37 @@
+import { baseApi } from "../../api/baseApi";
+
+const courseManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // getAllSemesters: builder.query({
+    //   query: (args) => {
+    //     const params = new URLSearchParams();
+    //     if (args) {
+    //       // params.append(args[0].name, args[0].value);
+    //       args.forEach((item) => {
+    //         params.append(item.name, item.value);
+    //       });
+    //     }
+    //     return {
+    //       url: "academic-semesters",
+    //       method: "GET",
+    //       params: params,
+    //     };
+    //   },
+    //   transformResponse: (response: TResponseRedux<TAcademecSemester[]>) => {
+    //     return {
+    //       data: response.data,
+    //       meta: response.meta,
+    //     };
+    //   },
+    // }),
+    addRegisteredSemester: builder.mutation({
+      query: (data) => ({
+        url: "/semester-registrations/create-semester-registration",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useAddRegisteredSemesterMutation } = courseManagementApi;
